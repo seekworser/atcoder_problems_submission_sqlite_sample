2974a596b27b581c73a43a34d495308858ff1453
seekworser <seekworser1963@gmail.com>
2023-05-03 18:26:43 +0900

    minor fix, add note for dzip

--- read.py
+++ read.py
@@ -1,5 +1,8 @@
-import sqlite3, csv
+import os
+import sqlite3, csv, gzip
 
+if (os.path.exists("submissions.db")):
+    os.remove("submissions.db")
 con = sqlite3.connect("submissions.db")
 cursor = con.cursor()
 cursor.execute("""
@@ -17,7 +20,8 @@ CREATE TABLE submissions(
 )
 """)
 cnt = 0
-with open("submissions.csv") as f:
+# with gzip.open("submissions.csv.gz", "rt") as f:  # gzから直接も読めるけど、実行時間がちょっと長い
+with open("submissions.csv.gz") as f:
     reader = csv.reader(f)
     cursor.executemany("INSERT INTO submissions VALUES (?,?,?,?,?,?,?,?,?,?)", reader)
 
